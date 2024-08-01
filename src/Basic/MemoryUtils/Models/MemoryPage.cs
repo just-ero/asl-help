a7c511b8b70717037e2b7230a1828a92b1a4233e
Ero <imjustero@gmail.com>
2024-08-01 18:45:28 +0200

    Fix MemoryPage size

--- src/Basic/MemoryUtils/Models/MemoryPage.cs
+++ src/Basic/MemoryUtils/Models/MemoryPage.cs
@@ -5,14 +5,14 @@ public record MemoryPage
     internal unsafe MemoryPage(MEMORY_BASIC_INFORMATION mbi)
     {
         Base = (nint)mbi.BaseAddress;
-        RegionSize = (int)mbi.RegionSize;
+        RegionSize = (nint)mbi.RegionSize;
         Protect = mbi.Protect;
         State = mbi.State;
         Type = mbi.Type;
     }
 
     public nint Base { get; }
-    public int RegionSize { get; }
+    public nint RegionSize { get; }
 
     public MemPageProtect Protect { get; }
     public MemPageState State { get; }
