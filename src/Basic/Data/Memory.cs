1033f6814d4ae89adb7c46917e9b3bd2fd494c6c
Ero <imjustero@gmail.com>
2024-10-08 19:09:05 +0200

    wip

--- src/Basic/Data/Memory.cs
+++
@@ -1,6 +0,0 @@
-namespace AslHelp.Data;
-
-internal static class Memory
-{
-    public static Func<string, nint, nint> OnFound { get; } = (_, addr) => Basic.Instance.FromAssemblyAddress(addr);
-}
--- src/Basic/MemoryUtils/WinAPI/Enums/MemPageType.cs
+++
@@ -1,8 +0,0 @@
-namespace AslHelp.MemUtils;
-
-public enum MemPageType : uint
-{
-    MEM_PRIVATE = 0x0020000,
-    MEM_MAPPED = 0x0040000,
-    MEM_IMAGE = 0x1000000
-}
